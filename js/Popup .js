a5f6b431f811416c85e6d37f2f26185b837ea317
jrgit168 <gitpen34@gmail.com>
2023-08-18 11:30:22 +0800

    將多餘的程式碼刪除，並重新整理程式

---
+++ js/Popup .js
@@ -0,0 +1,12 @@
+function show_information(SNo) {
+    document.getElementById("show_click").click();
+    $(".modal-content").load();
+  }
+  function edit_new_record(SNo) {
+    document.getElementById("edit_new_click").click();
+    $(".modal-content").load();
+  }
+  function edit_record(SNo) {
+    document.getElementById("edit_click").click();
+    $(".modal-content").load();
+  }
