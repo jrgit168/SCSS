7f506744dd34ab5b1eec0d54faff7d66c64515c6
jrgit168 <gitpen34@gmail.com>
2023-08-24 14:55:11 +0800

    3.暫定沒有要使用pug、傑人線上家教網站原碼

--- js/ForNewJR_Functional/NewJR_Functional.js
+++ js/ForNewJR_Functional/NewJR_Functional.js
@@ -1,9 +1,18 @@
 
-document.write('<script src="./js/ForNewJR_Functional/js/jquery.js"></script>');
-document.write('<script src="./js/ForNewJR_Functional/js/jquery-2.1.1.min.js"></script>');
-document.write('<script src="./js/ForNewJR_Functional/js/jquery.nicescroll.js"></script>');
-document.write('<script src="./js/ForNewJR_Functional/js/jquery.magnific-popup.js" type="text/javascript"></script>');
-document.write('<script src="./js/ForNewJR_Functional/js/bootstrap.js"> </script>');
-document.write('<script src="./js/ForNewJR_Functional/js/jquery-ui.js"></script>');
-document.write('<script src="./js/ForNewJR_Functional/js/datepicker-zh-TW.js"></script>');
-document.write('<script src="./js/ForNewJR_Functional/js/scripts.js"></script>');
+document.write('<script src="../js/ForNewJR_Functional/js/jquery.js"></script>');
+document.write('<script src="../js/ForNewJR_Functional/js/jquery-2.1.1.min.js"></script>');
+document.write('<script src="../js/ForNewJR_Functional/js/jquery.nicescroll.js"></script>');
+document.write('<script src="../js/ForNewJR_Functional/js/jquery.magnific-popup.js" type="text/javascript"></script>');
+document.write('<script src="../js/ForNewJR_Functional/js/bootstrap.js"> </script>');
+document.write('<script src="../js/ForNewJR_Functional/js/jquery-ui.js"></script>');
+document.write('<script src="../js/ForNewJR_Functional/js/datepicker-zh-TW.js"></script>');
+document.write('<script src="../js/ForNewJR_Functional/js/scripts.js"></script>');
+
+// document.write('<script src="./js/ForNewJR_Functional/js/jquery.js"></script>');
+// document.write('<script src="./js/ForNewJR_Functional/js/jquery-2.1.1.min.js"></script>');
+// document.write('<script src="./js/ForNewJR_Functional/js/jquery.nicescroll.js"></script>');
+// document.write('<script src="./js/ForNewJR_Functional/js/jquery.magnific-popup.js" type="text/javascript"></script>');
+// document.write('<script src="./js/ForNewJR_Functional/js/bootstrap.js"> </script>');
+// document.write('<script src="./js/ForNewJR_Functional/js/jquery-ui.js"></script>');
+// document.write('<script src="./js/ForNewJR_Functional/js/datepicker-zh-TW.js"></script>');
+// document.write('<script src="./js/ForNewJR_Functional/js/scripts.js"></script>');
